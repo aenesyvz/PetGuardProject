15b990de6e1327b7bbd53aba2fa968986ea36ca8
aenesyvz <20060403@stu.omu.edu.tr>
2024-08-17 23:38:34 +0300

    feat: Added controllers for city and district entities

---
+++ src/petGuardProject/WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,55 @@
+﻿using Application.Features.Cities.Commands.Create;
+using Application.Features.Cities.Commands.Delete;
+using Application.Features.Cities.Commands.Update;
+using Application.Features.Cities.Queries.GetAll;
+using Application.Features.Cities.Queries.GetById;
+using Core.Application.Requests;
+using Core.Application.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CitiesController : BaseController
+{
+    [HttpPost("Add")]
+    public async Task<IActionResult> Add([FromBody] CreateCityCommand createCityCommand)
+    {
+        CreatedCityResponse response = await Mediator.Send(createCityCommand);
+
+        return Created(uri: "", response);
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Update([FromBody] UpdateCityCommand updateCityCommand)
+    {
+        UpdatedCityResponse response = await Mediator.Send(updateCityCommand);
+
+        return Ok(response);
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        DeletedCityResponse response = await Mediator.Send(new DeleteCityCommand { Id = id });
+
+        return Ok(response);
+    }
+
+    [HttpGet("GetById/{id}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        GetByIdCityResponse response = await Mediator.Send(new GetByIdCityQuery { Id = id });
+        return Ok(response);
+    }
+
+    [HttpGet("GetListWithOutPagination")]
+    public async Task<IActionResult> GetListWithOutPagination()
+    {
+        GetAllCityQuery getListCityWithOutPaginationQuery = new();
+        IList<GetAllCityListItemDto> response = await Mediator.Send(getListCityWithOutPaginationQuery);
+        return Ok(response);
+    }
+}
---
+++ src/petGuardProject/WebAPI/Controllers/DistrictsController.cs
@@ -0,0 +1,66 @@
+﻿using Application.Features.Districts.Commands.Create;
+using Application.Features.Districts.Commands.Delete;
+using Application.Features.Districts.Commands.Update;
+using Application.Features.Districts.Queries.GetAllByDynamic;
+using Application.Features.Districts.Queries.GetById;
+using Application.Features.Districts.Queries.GetListByDynamic;
+using Core.Application.Requests;
+using Core.Application.Responses;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DistrictsController : BaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateDistrictCommand createDistrictCommand)
+    {
+        CreatedDistrictResponse response = await Mediator.Send(createDistrictCommand);
+
+        return Created(uri: "", response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateDistrictCommand updateDistrictCommand)
+    {
+        UpdatedDistrictResponse response = await Mediator.Send(updateDistrictCommand);
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        DeletedDistrictResponse response = await Mediator.Send(new DeleteDistrictCommand { Id = id });
+
+        return Ok(response);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        GetByIdDistrictResponse response = await Mediator.Send(new GetByIdDistrictQuery { Id = id });
+        return Ok(response);
+    }
+
+
+    [HttpPost("GetList/ByDynamic")]
+    public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery? dynamicQuery = null)
+    {
+        GetListDistrictByDynamicModelQuery listDistrictByDynamicModelQuery = new() { PageRequest = pageRequest, DynamicQuery = dynamicQuery };
+        GetListResponse<GetListDistrictByDynamicModelListItemDto> result = await Mediator.Send(listDistrictByDynamicModelQuery);
+        return Ok(result);
+    }
+
+    [HttpPost("GetListWithOutPagination/ByDynamic")]
+    public async Task<IActionResult> GetListWithOutPaginationByDynamic([FromBody] DynamicQuery? dynamicQuery = null)
+    {
+        GetAllDistrictByDynamicModelQuery listDistrictByDynamicModelQuery = new() { DynamicQuery = dynamicQuery };
+        IList<GetAllDistrictByDynamicModelListItemDto> result = await Mediator.Send(listDistrictByDynamicModelQuery);
+        return Ok(result);
+    }
+}
